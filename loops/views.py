5fd4a3a42a6d03d6394873e7ff1c2fd67877e16b
Marvens Longchamp <Longchampmarvens0@gmail.com>
2026-09-03 15:48:08 -0400

    superusers and admin can only edit bus loops

--- loops/views.py
+++ loops/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import BusSlot, Looptype
 from .forms import BusSlotUpdateForm
 
 # Create your views here.
+
+def is_staff_or_admin(user):
+    return user.is_staff or getattr(user, 'role', '') in ['ADMIN']
+
 
 def loop_dashboard(request):
     # Fetch slots for each loop, ordered by slot_number (handled automatically by Meta ordering)
@@ -16,12 +21,16 @@ def loop_dashboard(request):
 
     return render(request, 'loops/dashboard.html', context)
 
+
+# Restrict slot modifcation to loggin-in admins
+@login_required
+@user_passes_test(is_staff_or_admin)
 def update_slot(request, slot_id):
     slot = get_object_or_404(BusSlot, id = slot_id)
 
     if request.method == 'POST':
         form = BusSlotUpdateForm(request.POST,instance = slot)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
             return redirect('loops:dashboard')
     else:
